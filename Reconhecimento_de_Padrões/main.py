93e5f31007a94ae4c437c92dfa765ecbe71cfa12
akira saito <akirassan@hotmail.com>
2023-06-15 20:13:06 -0300

    fim

--- Reconhecimento_de_Padrões/main.py
+++ Reconhecimento_de_Padrões/main.py
@@ -23,12 +23,26 @@ for imagem in imagens:
     img1 = cv.cvtColor(cv.imread(caminho_imagem), cv.COLOR_BGR2GRAY)
     _, img1 = cv.threshold(img1, 120, 255, cv.THRESH_BINARY_INV)
 
+    #
+    # DESCRITORES DE BORDA
+    #
     contorno,ordem = cv.findContours(img1,cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     perimetro = cv.arcLength(contorno[0],True)
     perimetros.append(perimetro)
 
     diametro = np.sqrt(4*cv.contourArea(contorno[0])/np.pi)
     diametros.append(diametro)
+
+    #
+    # DESCRITORES DE REGIÃO
+    #
+    area = cv.countNonZero(img1)
+
+    compacidade = np.square(perimetro)/area
+
+    # Momentos de Hu
+    momentos = cv.moments(img1)
+    momentos_hu = cv.HuMoments(momentos)
 
     for i in range(0, len(tipo_doc)):
         imagem = imagem.replace(tipo_doc[i], "")
@@ -58,8 +72,51 @@ for imagem in imagens:
 
 dados = {'#': numero,
          'perimetro': perimetros,
-         'diametro': diametros}
+         'diametro': diametros,
+         'frutas':nome_fruta}
 df = pd.DataFrame(dados)
 nome_arquivo = 'dados.xlsx'
 df.to_excel(nome_arquivo)
+# %%
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+
+#importar o dataset
+dados = pd.read_excel('dados.xlsx')
+
+#separas as 2 (duas) características e os nomes das espécies (classes)
+X = dados.iloc[:,1:3]   # colunas de zero até três
+y = dados.iloc[:,3]     # coluna quatro
+
+#
+# PRÉ-PROCESSAMENTO
+#
+normalizar = StandardScaler()
+normalizar.fit(X)
+X = normalizar.transform(X)
+
+XTrain, XTest, yTrain, yTest = train_test_split(X,y,test_size=0.3)
+
+print('Tamanho do dataset de TRAIN: {0}'.format(len(XTrain)))
+print('Tamanho do dataset de TEST: {0}'.format(len(XTest)))
+
+#
+# CLASSIFICADOR
+#
+knn = KNeighborsClassifier(n_neighbors=3)
+lab = preprocessing.LabelEncoder()
+y_transformed = lab.fit_transform(yTrain)
+knn.fit(XTrain,y_transformed)
+Y = knn.predict(XTest).round()
+
+# RESULTADO
+acc = accuracy_score(yTest,Y)
+print('Acurácia: {0:.2f} ({1:.2f}%) \n\n'.format(acc,(acc*100)))
+
+# MATRIZ CONFUSÃO
+print(pd.crosstab(yTest,Y,rownames=['True'],colnames=['Predição'],margins=True))
 # %%
